36ec4b83a08f436281b43ecd419774325b74e309
indira-coopts <indira.endaya@co-opts.com>
2024-06-21 19:32:35 +0100

    correction on root

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client"; // Use createRoot from react-dom/client
 import App from "./App.jsx"; // Ensure the correct import of App
 import "./WordCloud.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.createRoot(document.getElementById("root")).render(<App />); // Use createRoot and render
+
+//ReactDOM.render(<App />, document.getElementById("root"));
