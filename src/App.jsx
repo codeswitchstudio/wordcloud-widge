ac3648c461cd5be27ca96254eab71591447685f9
indira-coopts <indira.endaya@co-opts.com>
2024-06-21 16:48:27 +0100

    testing plugin

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { render } from "react-dom";
+import WordCloud from "react-d3-cloud";
 
-function App() {
-  const [count, setCount] = useState(0)
+const data = [
+  { text: "Hey", value: 1000 },
+  { text: "lol", value: 200 },
+  { text: "first impression", value: 800 },
+  { text: "very cool", value: 1000000 },
+  { text: "duck", value: 10 },
+];
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+render(<WordCloud data={data} />, document.getElementById("root"));
 
-export default App
+export default App;
